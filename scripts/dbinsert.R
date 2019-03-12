1efa3ec4e379808fc2555b581e47ba19ff922a5d
Paddy Tobias <paddy.tobias@deakin.edu.au>
2019-03-12 14:32:04 +1100

    update dirs

--- scripts/dbinsert.R
+++ scripts/dbinsert.R
@@ -3,5 +3,10 @@ if (!exists("res")){
   source("scripts/post_process.R")
 }
 
+if ("./database/" %in% list.dirs()){
+  dir.create("database/")
+}
+
+
 con = dbConnect(SQLite(), "database/job.db")
 dbWriteTable(con, name = "job", res_processed, append = T)
